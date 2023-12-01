8e7acfcd1e391a858eb9f14dcf9b6c0608201e55
G4tes <50048258+G4tes@users.noreply.github.com>
2023-12-01 20:07:32 +0700

    Add Toaster component and import it in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Provider } from "react-redux";
-import { persistor, store } from "./stores/index.ts";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from './stores/index.ts';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Toaster } from './components/ui/toaster.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
+        <Toaster />
       </PersistGate>
     </Provider>
   </React.StrictMode>
