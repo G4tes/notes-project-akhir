baad44d55014fa9d1f249c8165cbb8f9946c07d0
G4tes <50048258+G4tes@users.noreply.github.com>
2023-12-06 21:34:24 +0700

    Adjust height of message container,uncomment image

--- src/components/notes/message.tsx
+++ src/components/notes/message.tsx
@@ -35,36 +35,38 @@ function Message() {
   // console.log(NotesOpen, 'mssg');
 
   return (
-    <div className="flex flex-col-reverse justify-between w-full h-[94vh]">
+    <div className="flex flex-col-reverse justify-between w-full h-[93vh]">
       {/* footer */}
       <FooterMessage isLoading={isLoading} setIsLoading={setIsLoading} />
       {/* main */}
       {NotesOpen && (
-        <div className="flex flex-col w-full -mb-6">
-          <div className="flex flex-row justify-between px-8 ">
-            <div className="w-1/3 flex flex-row gap-4">
-              <button>
-                <Pin className="text-zinc-300" />
-              </button>
-              <button>
-                <ArrowDownToLine className="text-zinc-300" />
-              </button>
-            </div>
-            <div className="w-full flex flex-row justify-end gap-10">
-              <button className="text-zinc-300">Updates</button>
-              <button className="text-zinc-300">Share</button>
-              <button className="text-zinc-300">
-                <MoreHorizontal />
-              </button>
+        <div className="h-screen -mb-16">
+          <div className="flex flex-col w-full ">
+            <div className="flex flex-row justify-between px-8 ">
+              <div className="w-1/3 flex flex-row gap-4 fixed">
+                <button>
+                  <Pin className="text-zinc-300" />
+                </button>
+                <button>
+                  <ArrowDownToLine className="text-zinc-300" />
+                </button>
+              </div>
+              <div className="w-full flex flex-row justify-end gap-4">
+                <button className="text-zinc-300">Updates</button>
+                <button className="text-zinc-300">Share</button>
+                <button className="text-zinc-300">
+                  <MoreHorizontal />
+                </button>
+              </div>
             </div>
           </div>
           <div className=" h-full ">
-            <div className="flex items-center justify-center ">
+            {/* <div className="flex items-center  justify-center ">
               <div className="h-full w-2/3">
                 <img className="w-full h-full object-cover rounded-3xl" src="/mainmssg2.png" alt="" />
               </div>
-            </div>
-            <div key={dateNotes} className="pl-8 flex flex-col gap-4  ">
+            </div> */}
+            <div key={dateNotes} className="pl-8 flex flex-col gap-4  mt-10">
               <div className="flex flex-col gap-1">
                 <h1 className="text-3xl font-semibold">{titleNotes}</h1>
                 <div className="flex flex-row w-full mt-4">
@@ -83,7 +85,7 @@ function Message() {
                 {/* <p className="text-sm">By G4tes · 19 hours ago</p> */}
               </div>
               <p
-                className="overflow-y-auto  h-[25vh]"
+                className="overflow-y-auto h-[60vh]"
                 dangerouslySetInnerHTML={{
                   __html: contentNotes
                 }}
